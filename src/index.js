01b57b1b0d26f73a1167f04afd591b9e5e8f3d2d
Benedictia <179542386+Benedictia@users.noreply.github.com>
2024-11-14 18:55:03 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-// src/index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';  // Import the App component
-import './index.css';  // Import global styles (if you have one)
-
+import App from './App'; 
+import './index.css'; 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')  // This matches the div in public/index.html
+  document.getElementById('root') 
 );
